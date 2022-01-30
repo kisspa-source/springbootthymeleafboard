943f88098d48ca6070bacafc6b91adb028c28d8d
true4 <true4pjh@naver.com>
2022-01-30 23:15:33 +0900

    test

---
+++ src/test/java/com/tistory/hitomis/springbootthymeleafboard/controller/LoginControllerTest.java
@@ -0,0 +1,21 @@
+package com.tistory.hitomis.springbootthymeleafboard.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @Test
+    void loginView() {
+    }
+
+    @Test
+    void login() {
+
+    }
+
+    @Test
+    void logout() {
+    }
+}
